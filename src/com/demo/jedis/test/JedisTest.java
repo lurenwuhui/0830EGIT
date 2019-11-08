cad8bd628a8838c747fbc3700fc1f2005e250549
john <john@haha.com>
2019-11-08 21:21:25 +0800

    update by ecplise

---
+++ src/com/demo/jedis/test/JedisTest.java
@@ -0,0 +1,19 @@
+package com.demo.jedis.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import redis.clients.jedis.Jedis;
+
+public class JedisTest {
+
+	@Test
+	public void test() {
+		Jedis jedis = new Jedis("192.168.255.6", 6379,60000);
+		String pong = jedis.ping();
+		System.out.println(pong);
+		jedis.close();
+	}
+
+}
